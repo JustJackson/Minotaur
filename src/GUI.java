227ff2c87801d3938879736dc5a5ddb6cdc44bc3
Sean Matthew <smatthe2@unca.edu>
2018-04-30 22:54:51 -0400

    Working version of GUI implemented with basic starter model maze

--- src/GUI.java
+++ src/GUI.java
@@ -1,13 +1,77 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 /**
  *
- * @author seanm
+ * @author Sean Matthew
  */
-public class GUI {
-    
+public class GUI extends JFrame {
+
+    /**
+     * Values: 0 = not-visited 1 = start 2 = wall 3 = visited 4 = current location 9 = end
+     */
+    int[][] maze
+            = {
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            {2, 1, 2, 0, 2, 0, 2, 0, 0, 0, 0, 0, 2},
+            {2, 0, 2, 0, 0, 0, 2, 0, 2, 2, 2, 0, 2},
+            {2, 0, 0, 0, 2, 2, 2, 0, 0, 0, 0, 0, 2},
+            {2, 0, 2, 0, 0, 0, 0, 0, 2, 2, 2, 0, 2},
+            {2, 0, 2, 0, 2, 2, 2, 0, 2, 0, 0, 0, 2},
+            {2, 0, 2, 0, 2, 0, 0, 0, 2, 2, 2, 0, 2},
+            {2, 0, 2, 0, 2, 2, 2, 0, 2, 9, 2, 0, 2},
+            {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 2},
+            {2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2}
+            };
+
+    public GUI() {
+        setTitle("Minotaur");
+        setSize(600, 600);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        
+        g.translate(100, 100);
+        for (int row = 0; row < maze.length; row++) {
+            for (int col = 0; col < maze[0].length; col++) {
+                Color color;
+                switch (maze[row][col]) {
+                    case 1:
+                        color = Color.BLUE; break;
+                    case 2:
+                        color = Color.BLACK; break;
+                    case 3:
+                        color = Color.YELLOW; break;
+                    case 4:
+                        color = Color.RED; break;
+                    case 9:
+                        color = Color.GREEN; break;
+                    default:
+                        color = Color.WHITE;
+                }
+                g.setColor(color);
+                g.fillRect(30 * col, 30 * row, 30, 30);
+                g.setColor(Color.BLACK);
+                g.drawRect(30 * col, 30 * row, 30, 30);
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(new Runnable(){
+            @Override
+            public void run(){
+                GUI gui = new GUI();
+                gui.setVisible(true);
+            }
+        
+    });
+    }
 }

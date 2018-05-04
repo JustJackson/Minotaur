4c7c69f74b6a4a8206a8478eb9f756bf99b27c36
Sean Matthew <smatthe2@unca.edu>
2018-05-04 17:11:09 -0400

    added getCell and setCell methods to mazeRunner, plus functions to attach the gui and update the display.  You can now see solutions traversed in the gui... for some reason it's not setting the cells that it's leaving right now, though.  I'll work on that.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static void main(String[] args){
         MazeRunner mazeRunner = new MazeRunner();
         GUI gui = new GUI(mazeRunner); 
+        mazeRunner.attachGUI(gui);
 
     }
     
--- src/ManualSolver.java
+++ src/ManualSolver.java
@@ -6,8 +6,7 @@ import javax.swing.JTextField;
 public class ManualSolver implements KeyListener {
     //where initialization occurs:
     MazeRunner mazeRunner;
-    int currentX;
-    int currentY;
+    int currentX, currentY;
     
     public ManualSolver(MazeRunner mazeRunner){
         System.out.println("Entering Manual Solver");
@@ -30,15 +29,19 @@ public class ManualSolver implements KeyListener {
         if (e.getKeyChar() == 'w'){
             System.out.println("Move Up");
             System.out.println("Current Location is: (" + currentX + ", " + currentY + ")");
+            attemptMove(currentX, currentY-1);
         }else if(e.getKeyChar() == 's'){
             System.out.println("Move Down");
             System.out.println("Current Location is: (" + currentX + ", " + currentY + ")");
+            attemptMove(currentX, currentY+1);
         }else if(e.getKeyChar() == 'a'){
             System.out.println("Move Left");
-            System.out.println("Current Location is: (" + currentX + ", " + currentY + ")");    
+            System.out.println("Current Location is: (" + currentX + ", " + currentY + ")");
+            attemptMove(currentX-1, currentY);
         }else if (e.getKeyChar() == 'd'){
             System.out.println("Move Right");
             System.out.println("Current Location is: (" + currentX + ", " + currentY + ")");
+            attemptMove(currentX+1, currentY);
         }
     }
 
@@ -50,6 +53,22 @@ public class ManualSolver implements KeyListener {
     @Override
     public void keyReleased(KeyEvent e) {
         //Do Nothing
+    }
+    
+    private void attemptMove(int x, int y){
+        System.out.println("Attempting to move to (" + x + ", " + y + ")");
+        if (mazeRunner.getCell(x, y) == 9){
+            System.out.println("YOU WIN!");
+            System.exit(0);
+        }
+        if (mazeRunner.getCell(x, y) != 2){
+            mazeRunner.setCell(x, y, 4);
+            mazeRunner.setCell(currentX, currentY, 3);
+            currentX = x;
+            currentY = y;
+            mazeRunner.updateDisplay();
+        }else
+            System.out.println("Invalid move: Wall");
     }
 
     
